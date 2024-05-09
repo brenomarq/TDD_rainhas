7555bf7a106162ee0fc19537c2cae3ab5d813c42
brenomarq <brenomarques516@gmail.com>
2024-05-09 16:28:19 -0300

    Função de ler arquivo criada

--- rainhas.cpp
+++ rainhas.cpp
@@ -1,5 +1,36 @@
+// Copyright 2024 Breno
+#include "./rainhas.hpp"
+#include <iostream>
 #include <string>
+#include <vector>
+#include <fstream>
+
+std::vector<std::vector<int>> tabuleiro_para_vetor(
+    const std::string file_name
+) {
+    std::vector<std::vector<int>> tabuleiro;
+
+    // Abre o arquivo
+    std::ifstream file(file_name);
+
+    // Transforma cada elemento em int e adiciona em um vetor de vetores
+    std::string line;
+    while (std::getline(file, line)) {
+        std::vector<int> row;
+        for (char c : line) {
+            row.push_back(c - '0');
+        }
+        tabuleiro.push_back(row);
+    }
+
+    file.close();
+
+    return tabuleiro;
+}
 
 int checa_tabuleiro(const std::string file_name) {
+    // Gera o tabuleiro a ser analisado
+    std::vector<std::vector<int>> tabuleiro = tabuleiro_para_vetor(file_name);
+
     return 0;
 }
