491069f2370709fa357ceb5817364bfe38a1bc6a
brenomarq <brenomarques516@gmail.com>
2024-05-16 20:44:47 -0300

    Teste tabuleiro 3 criado

--- testa_rainhas.cpp
+++ testa_rainhas.cpp
@@ -7,3 +7,7 @@ TEST_CASE("Tabuleiro - válido e solução", "[rainhas]") {
     REQUIRE(checa_tabuleiro("tabuleiro1.txt") == 1);
     REQUIRE(checa_tabuleiro("tabuleiro2.txt") == 1);
 }
+
+TEST_CASE("Tabuleiro - válido, mas não é solução", "[rainhas]") {
+    REQUIRE(checa_tabuleiro("Tabuleiro3.txt") == 0);
+}
